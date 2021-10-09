5f8a2e60233a8a3d9dcd9043f1ba429a045502d9
Marco Serrano <marcoTheWebDev@gmail.com>
2021-10-09 13:23:41 -0500

    Added an alert in case Metamask is on the wrong network.

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -2,15 +2,22 @@ import Web3 from 'web3';
 
 let web3;
 
-if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-    window.ethereum.request({ method: 'eth_requestAccounts' });
-    web3 = new Web3(window.ethereum);
-} else {
-    const provider = new Web3.providers.HttpProvider(
-        process.env.INFURA_ENDPOINT
-    );
+(async () => {
+    if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+        window.ethereum.request({ method: 'eth_requestAccounts' });
+        web3 = new Web3(window.ethereum);
 
-    web3 = new Web3(provider);
-}
+        const chainId = await web3.eth.net.getId();
+        if (chainId != 4) {
+            alert('You need to be connected to the Rinkeby network in Metamask in order to use this dApp.');
+        }
+    } else {
+        const provider = new Web3.providers.HttpProvider(
+            process.env.INFURA_ENDPOINT
+        );
+    
+        web3 = new Web3(provider);
+    }
+})();
 
 export default web3;
